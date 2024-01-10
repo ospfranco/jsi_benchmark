7d83ac599d4603a67943f5875e305ac4c002b6a3
Oscar Franco <ospfranco@gmail.com>
2024-01-10 17:17:05 +0100

    Readme

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # JSI benchmark
 
-# iOS Release Mode on m2 max
+# iOS iPhone 15 Pro release
 
 ![ios](ios.png)
+
+# Android Samsung S22 release
+![android](android.jpg)
---
+++ modules/my-module/android/build/generated/source/buildConfig/release/expo/modules/mymodule/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package expo.modules.mymodule;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "expo.modules.mymodule";
+  public static final String BUILD_TYPE = "release";
+}
